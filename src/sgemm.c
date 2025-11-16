ed730d7ca932f927ca39d0d2cd878f4bd48c30ac
Vinayak Dev <vinayakdev.sci@gmail.com>
2025-11-16 09:39:09 +0000

    Wire up the build system
    Signed-off-by: Vinayak Dev <vinayakdev.sci@gmail.com>

---
+++ src/sgemm.c
@@ -0,0 +1,8 @@
+#include "devblas/sgemm.h"
+#include <stdio.h>
+
+
+
+void sgemm(float *A, float *B, float *C, int M, int N, int K) {
+	fprintf(stderr, "WORKS!");
+}
---
+++ tests/test_sgemm.cc
@@ -0,0 +1,4 @@
+#include "devblas/sgemm.h"
+#include <gtest/gtest.h>
+
+TEST(GemmTest, Setup) { ASSERT_EQ(0, 0); }
